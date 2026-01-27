1cf54ce4588ea44fdb74ecf09bb43eb5eddc07e6
Arjun Kadam <153195543+codewitharjun214@users.noreply.github.com>
2026-01-27 20:20:06 +0530

    Day 1: Python Topic Covered

---
+++ conditional.py
@@ -0,0 +1,17 @@
+#write a code for grade system using conditional statements 
+
+marks = 75
+
+if(marks >= 90):
+   grade = "A"
+
+elif(marks >= 80 and marks < 90):
+    grade = "B"
+
+elif(marks >= 70 and marks < 80):
+    grade = "c"
+
+else:
+    grade = "D"
+
+print("grade of the student :" , grade)
---
+++ strfunction.py
@@ -0,0 +1,4 @@
+# write a program to input users name and print its length 
+
+name = input("Enter Your Name :")
+print(len(name))
